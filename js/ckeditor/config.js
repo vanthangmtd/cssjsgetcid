1bf48b6a3415018d65ca9eaed19988da9646f133
vanthangmtd <vanthangmtd@gmail.com>
2021-08-14 19:29:50 +0700

    commit

---
+++ js/ckeditor/config.js
@@ -0,0 +1,20 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+    config.toolbar = [
+        { name: 'document', items: ['Save', 'NewPage', 'ExportPdf', 'Preview', 'Print', '-', 'Templates'] },
+        { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+        { name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'] },
+        { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat'] },
+        { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language'] },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+        { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe'] },
+        { name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
+        { name: 'colors', items: ['TextColor', 'BGColor'] },
+    ];
+    config.enterMode = CKEDITOR.ENTER_DIV;
+    config.shiftEnterMode = CKEDITOR.ENTER_BR;
+};
---
+++ js/ckeditor/samples/js/sample.js
@@ -0,0 +1,86 @@
+﻿/**
+ * Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* exported initSample */
+
+if ( CKEDITOR.env.ie && CKEDITOR.env.version < 9 )
+	CKEDITOR.tools.enableHtml5Elements( document );
+
+// The trick to keep the editor in the sample quite small
+// unless user specified own height.
+CKEDITOR.config.height = 700;
+CKEDITOR.config.width = 'auto';
+
+var initSampleEncode = ( function() {
+	var wysiwygareaAvailable = isWysiwygareaAvailable(),
+		isBBCodeBuiltIn = !!CKEDITOR.plugins.get( 'bbcode' );
+
+	return function() {
+		var editorElement = CKEDITOR.document.getById( 'editorEncode' );
+
+		// :(((
+		if ( isBBCodeBuiltIn ) {
+			editorElement.setHtml();
+		}
+
+		// Depending on the wysiwygarea plugin availability initialize classic or inline editor.
+		if ( wysiwygareaAvailable ) {
+			CKEDITOR.replace( 'editorEncode' );
+		} else {
+			editorElement.setAttribute( 'contenteditable', 'true' );
+			CKEDITOR.inline( 'editorEncode' );
+
+			// TODO we can consider displaying some info box that
+			// without wysiwygarea the classic editor may not work.
+		}
+	};
+
+	function isWysiwygareaAvailable() {
+		// If in development mode, then the wysiwygarea must be available.
+		// Split REV into two strings so builder does not replace it :D.
+		if ( CKEDITOR.revision == ( '%RE' + 'V%' ) ) {
+			return true;
+		}
+
+		return !!CKEDITOR.plugins.get( 'wysiwygarea' );
+	}
+})();
+
+
+var initSampleDecode = (function () {
+	var wysiwygareaAvailable = isWysiwygareaAvailable(),
+		isBBCodeBuiltIn = !!CKEDITOR.plugins.get('bbcode');
+
+	return function () {
+		var editorElement = CKEDITOR.document.getById('editorDecode');
+
+		// :(((
+		if (isBBCodeBuiltIn) {
+			editorElement.setHtml();
+		}
+
+		// Depending on the wysiwygarea plugin availability initialize classic or inline editor.
+		if (wysiwygareaAvailable) {
+			CKEDITOR.replace('editorDecode');
+		} else {
+			editorElement.setAttribute('contenteditable', 'true');
+			CKEDITOR.inline('editorDecode');
+
+			// TODO we can consider displaying some info box that
+			// without wysiwygarea the classic editor may not work.
+		}
+	};
+
+	function isWysiwygareaAvailable() {
+		// If in development mode, then the wysiwygarea must be available.
+		// Split REV into two strings so builder does not replace it :D.
+		if (CKEDITOR.revision == ('%RE' + 'V%')) {
+			return true;
+		}
+
+		return !!CKEDITOR.plugins.get('wysiwygarea');
+	}
+})();
+
